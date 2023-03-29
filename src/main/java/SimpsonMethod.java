6b313c4dc3b2222f34947fd987e57287291468e6
Ususi <334578@niuitmo.ru>
2023-03-29 16:24:46 +0300

    Final Lab 3

---
+++ src/main/java/SimpsonMethod.java
@@ -0,0 +1,66 @@
+public class SimpsonMethod {
+    int variant = 1;
+
+    public void setVariant(int variant) {
+        this.variant = variant;
+    }
+
+    public double integrate(double a, double b) throws MyException {
+        int n = 1000000;
+        int i,z;
+        double h,s;
+        double e = 0.0000001;
+        n=n+n;
+        s = f(a)*f(b);
+        h = (b-a)/n;
+        z = 4;
+
+        // если начинаем интегрировать с точки разрыва, то s становится NaN и все ломает
+        if(Double.isNaN(s)){
+            s = f(a+h-e)*f(b+h-e);
+            System.out.println("Разрыв устранен.\n" +
+                    "Взято среднее от значений от двух соседних точках разрыва в точке 0.");
+        }
+
+        if (variant == 1 & ((a<b & b<0) | (b>0 & a>b))){
+            for(i = 1; i<n; i++){
+                double perem = f(a+i*h);
+                s = s + z * perem;
+                z = 6 - z;
+            }
+            return (s * h)/3;
+        }else if(variant != 1){
+            for(i = 1; i<n; i++){
+                double perem = f(a+i*h);
+                if(variant == 3 & (perem == Double.POSITIVE_INFINITY | perem == Double.NEGATIVE_INFINITY | Double.isNaN(perem))){
+                    if (a<=0 & b>=0){
+                        System.out.println("Разрыв устранен.\n" +
+                                "Взято среднее от значений от двух соседних точках разрыва в точке 0.");
+                        var nextperem = f(a+i*h + e);
+                        var privperem = f(a+i*h - e);
+                        perem = (nextperem+privperem)/2;
+                    }
+                }
+                s = s + z * perem;
+                z = 6 - z;
+            }
+            return (s * h)/3;
+        }else{
+            throw new MyException("Вы чаво? Мы не можем это интегрировать(\n" +
+                    "Это разрыв второго рода...");
+        }
+    }
+
+    private double f(double x) {
+        // здесь нужно указать функцию, которую мы интегрируем
+        if(variant == 1){
+            return 1/x;
+        }else if(variant == 2){
+            return x*x - x + 1;
+        }else if(variant == 3){
+            return Math.sin(x)/x;
+        } else{
+            return Math.cos(x);
+        }
+    }
+}
---
+++ src/main/java/Start.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+        SimpsonMethod simpsonMethod = new SimpsonMethod();
+        System.out.println("""
+                Выберите уравнение
+                1)1/x
+                2)x^2 - x + 1
+                3)sin(x)/x
+                4)cos(x)""");
+        Scanner scanner = new Scanner(System.in);
+        int variant = scanner.nextInt();
+        System.out.println("Введите границы интегрирования");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        simpsonMethod.setVariant(variant);
+        try {
+            Double answer = simpsonMethod.integrate(a, b);
+            System.out.println(answer);
+        } catch (MyException e){
+            System.out.println("Вы чаво? Мы не можем это интегрировать(\n" +
+                    "Это разрыв второго рода...");
+        }
+    }
+}
